499f7bb0a842d44b58b68bcbb01bd47c56c0ff99
Mustafa <maxusmusti@gmail.com>
2021-03-23 06:20:47 -0400

    Wordnet synset update for embeddings

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Detecting double-entendres in natural language
 Instructions:
  - `pip3 install -r requirements.txt`
  - Open a python interpreter, run `import nltk` then `nltk.download()`
- - Download "wordnet" and "stopwords" packages
+ - Download "wordnet", "stopwords", and "punkt" packages
  - Put sentences for detection in `sentences.txt`, seperated by newlines
  - `python3 main.py`
--- embedding.py
+++ embedding.py
@@ -1,16 +1,33 @@
 """
 This is where the code for creating embeddings will go
-Base Reference: https://www.analyticsvidhya.com/blog/2020/08/top-4-sentence-embedding-techniques-using-python/
+Base Reference: https://www.sbert.net/
 """
+import numpy as np
 from sentence_transformers import SentenceTransformer
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 class SentenceEmbedder:
     def __init__(self):
         self.model = SentenceTransformer('bert-base-nli-mean-tokens')
 
     def encode(self, sentences):
+        final_embeddings = []
         sentence_embeddings = self.model.encode(sentences)
-        return sentence_embeddings
+        for i in range(len(sentences)):
+            f1 = 0
+            anti1 = 0
+            for j in word_tokenize(sentences[i]):
+                defs = wn.synsets(j, 'v')
+                f1 += len(defs)
+                if len(defs) >= 1:
+                    anti1 += 1
+                #for synset in defs:
+                    #print(synset.definition())
+            final_embeddings.append(np.append(sentence_embeddings[i], f1/anti1))
+            #print(f1/anti1)
+
+
+        return final_embeddings
 
