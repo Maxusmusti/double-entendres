f965c1da740f01d76018b86bc143d9b8f1124832
Mustafa <maxusmusti@gmail.com>
2021-04-04 18:40:56 -0400

    Added examples and enhanced embeddings

--- embedding.py
+++ embedding.py
@@ -10,7 +10,9 @@ from nltk.tokenize import word_tokenize
 
 class SentenceEmbedder:
     def __init__(self):
-        self.model = SentenceTransformer('bert-base-nli-mean-tokens')
+        #self.model = SentenceTransformer('bert-base-nli-mean-tokens')
+        self.model = SentenceTransformer('stsb-roberta-large')
+        self.sw = stop_words = set(stopwords.words('english'))
 
     def encode(self, sentences):
         final_embeddings = []
@@ -18,17 +20,25 @@ class SentenceEmbedder:
         for i in range(len(sentences)):
             f1 = 0
             anti1 = 0
-            for j in word_tokenize(sentences[i]):
+            matches = 0
+            words = word_tokenize(sentences[i])
+            for j in words:
                 defs = wn.synsets(j, 'v')
                 f1 += len(defs)
                 if len(defs) >= 1:
                     anti1 += 1
-                #for synset in defs:
-                    #print(synset.definition())
-                    #print(synset.examples())
-            final_embeddings.append(np.append(sentence_embeddings[i], f1/anti1))
-            #print(f1/anti1)
-
+                for synset in defs:
+                    set_def = synset.definition()
+                    examples = synset.examples()
+                    for word in words:
+                        if word in self.sw:
+                            continue
+                        if word in set_def:
+                            matches += 1
+                        for example in examples:
+                            if word in example:
+                                matches += 1 
+            final_embeddings.append(np.append(np.append(sentence_embeddings[i], f1/anti1), matches))
 
         return final_embeddings
 
