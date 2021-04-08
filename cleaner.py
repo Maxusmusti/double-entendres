6235568ed49dc7da401155ff98a27052abe0b106
Andrew Calimlim <Andr3wCalimlim@gmail.com>
2021-04-07 17:57:44 -0700

    Add files via upload

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,7 @@
+import re
+
 #my_set = set(open('double_entendres.txt'))
+
 """
 with open('double_entendres.txt', 'r') as file:
     data = file.read()
@@ -12,18 +15,20 @@ for de in theSet:
 file1.close()
 """
 
-with open('de_pure.txt', 'r') as file:
+with open('normal.txt', 'r') as file:
     data = file.read()
+
+data = re.sub('\[.*\]\n', '', data)
 
 theSet = set(data.split('\n\n'))
 
 print(len(theSet))
 
-file1 = open("sentences.txt", "w")
+file1 = open("sentences_2.txt", "w")
 
 for de in theSet:
     de = de.replace("\n", ";")
-    file1.write(de + "\t1\n")
+    file1.write(de + "\t0\n")
 file1.close()
 
 #print(theSet)
