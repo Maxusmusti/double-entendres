4c2bb961b130c9b4a504cce4cd2de42c4488b154
Mustafa <maxusmusti@gmail.com>
2021-04-11 18:25:49 -0400

    Added max noun meaning count feature

--- embedding.py
+++ embedding.py
@@ -18,15 +18,25 @@ class SentenceEmbedder:
         final_embeddings = []
         sentence_embeddings = self.model.encode(sentences)
         for i in range(len(sentences)):
+            # Vars for feature building
             f1 = 0
             anti1 = 1
             matches = 0
+            max_n = 0
+
+            # Iterate through each word in the sentence
             words = word_tokenize(sentences[i])
             for j in words:
+                # Build average verb meaning count feature
                 defs = wn.synsets(j, 'v')
                 f1 += len(defs)
                 if len(defs) >= 1:
                     anti1 += 1
+                # Build max noun meaning count feature
+                if j not in self.sw:
+                    n_defs = wn.synsets(j, 'n')
+                    max_n = max(max_n, len(n_defs))
+                # Build total word to verb meaning matches feature
                 for synset in defs:
                     set_def = synset.definition()
                     examples = synset.examples()
@@ -38,7 +48,7 @@ class SentenceEmbedder:
                         for example in examples:
                             if word in example:
                                 matches += 1 
-            final_embeddings.append(np.append(np.append(sentence_embeddings[i], f1/anti1), matches))
+            final_embeddings.append(np.append(np.append(np.append(sentence_embeddings[i], f1/anti1), matches), max_n))
 
         return final_embeddings
 
